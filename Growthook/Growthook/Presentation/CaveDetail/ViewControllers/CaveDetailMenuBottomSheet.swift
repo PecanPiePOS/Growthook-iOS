4e8e296d3076e36e5acbb0e238e05ff90d80adf3
kwonjeong <kwonjeong9470@gmail.com>
2024-01-10 16:37:30 +0900

    [Feat] #48 - 동굴 메뉴 커스텀 버튼 구현

---
+++ Growthook/Growthook/Presentation/CaveDetail/ViewControllers/CaveDetailMenuBottomSheet.swift
@@ -0,0 +1,23 @@
+//
+//  CaveDetailMenuModal.swift
+//  Growthook
+//
+//  Created by KJ on 1/10/24.
+//
+
+import UIKit
+
+import Moya
+import SnapKit
+import Then
+import RxCocoa
+import RxSwift
+
+final class CaveDetailMenuBottomSheet: BaseViewController {
+
+    // MARK: - UI Components
+    
+    private lazy var changeCaveButton = UIButton()
+    private lazy var deleteCaveButton = UIButton()
+
+}
---
+++ Growthook/Growthook/Presentation/CaveDetail/Views/CaveMenuButton.swift
@@ -0,0 +1,48 @@
+//
+//  CaveMenuButton.swift
+//  Growthook
+//
+//  Created by KJ on 1/10/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class CaveMenuButton: UIButton {
+
+    // MARK: - UI Components
+    
+    private lazy var image = UIImage()
+    private lazy var title = AttributedString()
+    
+    
+    // MARK: - Initializer
+
+    init(buttonTitle: AttributedString, buttonImage: UIImage) {
+        super.init(frame: .zero)
+        self.title = buttonTitle
+        self.image = buttonImage
+        setStyle()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - UI Components Property
+    
+    func setStyle() {
+        
+        title.font = .fontGuide(.body1_reg)
+        
+        var config = UIButton.Configuration.filled()
+        config.attributedTitle = title
+        config.image = image
+        config.imagePadding = 12
+        config.baseBackgroundColor = .clear
+        config.imagePlacement = NSDirectionalRectEdge.leading
+        configuration = config
+    }
+}
