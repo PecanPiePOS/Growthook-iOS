995b4e1a0ab9b0deb3548570baa7842ede1fc906
kwonjeong <kwonjeong9470@gmail.com>
2024-01-11 03:23:20 +0900

    [Feat] #51 - 인사이트 알림 Dto 구현

---
+++ Growthook/Growthook/Data/Insight/GetSeedAlarmResponseDto.swift
@@ -0,0 +1,12 @@
+//
+//  GetSeedAlarmResponseDto.swift
+//  Growthook
+//
+//  Created by KJ on 1/11/24.
+//
+
+import Foundation
+
+struct GetSeedAlarmResponseDto: Codable {
+    let seedCount: Int
+}
