71b898bdaa5e1b7563ea716ed84bc2265f87b790
kwonjeong <kwonjeong9470@gmail.com>
2024-01-11 01:23:37 +0900

    [Feat] #51 - 인사이트 리스트 Service 구현

---
+++ Growthook/Growthook/Network/APIService/SeedListType.swift
@@ -0,0 +1,100 @@
+//
+//  SeedService.swift
+//  Growthook
+//
+//  Created by KJ on 1/11/24.
+//
+
+import Foundation
+
+import Moya
+import RxCocoa
+import RxMoya
+import RxSwift
+
+enum SeedListType {
+    case getSeedList(memberId: Int)
+    case getSeedListByCave(caveId: Int)
+    case getSeedAlarm(memberId: Int)
+    case deleteSeed(seedId: Int)
+    case patchSeed(seedId: Int)
+    case patchUnlockSeed(seedId: Int)
+    case postSeedMove(seedId: Int)
+    case patchSeedScrap(seedId: Int)
+}
+
+extension SeedListType: BaseTargetType {
+    
+    var path: String {
+        switch self {
+        case .getSeedList(memberId: let memberId):
+            let path = URLConstant.seedListGet
+                .replacingOccurrences(of: "{memberId}", with: String(memberId))
+            return path
+        case .getSeedListByCave(caveId: let caveId):
+            let path = URLConstant.seedListByCaveGet
+                .replacingOccurrences(of: "{caveId}", with: String(caveId))
+            return path
+        case .getSeedAlarm(memberId: let memberId):
+            let path = URLConstant.seedAlarm
+                .replacingOccurrences(of: "{memberId}", with: String(memberId))
+            return path
+        case .deleteSeed(seedId: let seedId):
+            let path = URLConstant.seed
+                .replacingOccurrences(of: "{seedId}", with: String(seedId))
+            return path
+        case .patchSeed(seedId: let seedId):
+            let path = URLConstant.seed
+                .replacingOccurrences(of: "{seedId}", with: String(seedId))
+            return path
+        case .patchUnlockSeed(seedId: let seedId):
+            let path = URLConstant.unLockSeed
+                .replacingOccurrences(of: "{seedId}", with: String(seedId))
+            return path
+        case .postSeedMove(seedId: let seedId):
+            let path = URLConstant.seedMove
+                .replacingOccurrences(of: "{seedId}", with: String(seedId))
+            return path
+        case .patchSeedScrap(seedId: let seedId):
+            let path = URLConstant.toggleSeedScrapStatus
+                .replacingOccurrences(of: "{seedId}", with: String(seedId))
+            return path
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .getSeedList, .getSeedListByCave, .getSeedAlarm:
+            return .get
+        case .postSeedMove:
+            return .post
+        case .deleteSeed:
+            return .delete
+        case .patchSeed, .patchSeedScrap, .patchUnlockSeed:
+            return .patch
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        default:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        return APIConstants.headerWithOutToken
+    }
+}
+
+struct SeedListService: Networkable {
+    typealias Target = SeedListType
+    private static let provider = makeProvider()
+    
+    static func getSeedList(with memberId: Int) -> Observable<GetSeedListResponseDto> {
+        return provider.rx.request(.getSeedList(memberId: memberId))
+            .asObservable()
+            .mapError()
+            .decode(decodeType: GetSeedListResponseDto.self)
+    }
+}
