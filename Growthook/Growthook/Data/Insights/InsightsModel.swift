12834f374153c55e990725f12c5aa30801f6c53b
PecanPie <hidra0321@naver.com>
2024-01-10 14:46:45 +0900

    [Feat] - #49 Insights Model 를 추가했습니다.
    1. Insights Text limit protocol 추가 및 구현체 InsightTextLimits 추가 2. PeriodModel 을 추가하여 maximumPeriod 만 변경하면, 내부 소스의 최대 개월 수 구현 3. Insight 작성에 필요한 모델들 추가

--- Growthook/Growthook/Data/Insights/InsightsModel.swift
+++ Growthook/Growthook/Data/Insights/InsightsModel.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+
+protocol TextLimits {
+    var textLimit: Int { get }
+}
+
+enum InsightTextLimits: TextLimits {
+    case insight
+    case memo
+    case reference
+    
+    var textLimit: Int {
+        switch self {
+        case .insight:
+            return 30
+        case .memo:
+            return 300
+        case .reference:
+            return 20
+        }
+    }
+}
+
+struct PeriodModel {
+    /// 변경 가능한 기한
+    private static let maximumPeriodMonthCount: Int = 12
+    
+    static var periodSetToSelect: [InsightPeriodModel] {
+        var periodSet: [InsightPeriodModel] = []
+        let defaultCase = InsightPeriodModel(periodMonthAsInteger: nil, periodTitle: "선택")
+        periodSet.append(defaultCase)
+        
+        for month in 1...maximumPeriodMonthCount {
+            let bodyPeriodModel = InsightPeriodModel(periodMonthAsInteger: month, periodTitle: "\(month)개월")
+            periodSet.append(bodyPeriodModel)
+        }
+        return periodSet
+    }
+}
+
+struct InsightCaveModel {
+    var caveId: Int
+    var caveTitle: String
+}
+
+struct InsightPeriodModel {
+    var periodMonthAsInteger: Int?
+    var periodTitle: String
+}
