4c20f937ebb9bcdd308b465e6632bc7c74a2d073
PecanPie <hidra0321@naver.com>
2024-01-10 14:49:35 +0900

    [Feat] - #49 Insights Service 를 구현했습니다.

--- Growthook/Growthook/Network/API/Insights/InsightsService.swift
+++ Growthook/Growthook/Network/API/Insights/InsightsService.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+
+import Moya
+import RxCocoa
+import RxMoya
+import RxSwift
+
+enum InsightsTarget {
+    case postNewInsight(caveId: Int, insightParameter: InsightPostRequest)
+}
+
+extension InsightsTarget: BaseTargetType {
+    
+    var headers: [String : String]? {
+        APIConstants.headerWithAuthorization
+    }
+    
+    var authorizationType: AuthorizationType? {
+        return .bearer
+    }
+    
+    var path: String {
+        switch self {
+        case .postNewInsight(let caveId, _):
+            let newPath: String = URLConstant.seedPost
+                .replacingOccurrences(of: "{caveId}", with: String(caveId))
+            return newPath
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .postNewInsight:
+            return .post
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .postNewInsight(_, let insightParameter):
+            let parameters = try! insightParameter.asParameter()
+            return .requestParameters(parameters: parameters, encoding: JSONEncoding.default)
+        }
+    }
+}
+
+struct InsightsService: Networkable {
+    typealias Target = InsightsTarget
+    private static let provider = makeProvider()
+    
+    static func postNewInsight(caveId: Int, of insight: InsightPostRequest) -> Observable<InsightPostResponse> {
+        return provider.rx.request(.postNewInsight(caveId: caveId, insightParameter: insight))
+            .asObservable()
+            .mapError()
+            .decode(decodeType: InsightPostResponse.self)
+    }
+    
+}
