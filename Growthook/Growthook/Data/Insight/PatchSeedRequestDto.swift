4522e3e3382a94981d29705ae17a604cd78fa1af
kwonjeong <kwonjeong9470@gmail.com>
2024-01-11 03:36:52 +0900

    [Feat] #51 - 씨앗 내용 수정 Dto 구현

---
+++ Growthook/Growthook/Data/Insight/PatchSeedRequestDto.swift
@@ -0,0 +1,15 @@
+//
+//  PatchSeedRequestDto.swift
+//  Growthook
+//
+//  Created by KJ on 1/11/24.
+//
+
+import Foundation
+
+struct PatchSeedRequestDto: Codable {
+    let insight: String
+    let memo: String
+    let source: String
+    let url: String
+}
