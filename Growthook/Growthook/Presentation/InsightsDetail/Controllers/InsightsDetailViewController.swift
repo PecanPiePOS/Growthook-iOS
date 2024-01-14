221f93e03b17181cdb69edae039b443745ab4e5f
PecanPie <hidra0321@naver.com>
2024-01-14 23:42:09 +0900

    인사이트 조회 뷰 구현

--- Growthook/Growthook/Presentation/InsightsDetail/Controllers/InsightsDetailViewController.swift
+++ Growthook/Growthook/Presentation/InsightsDetail/Controllers/InsightsDetailViewController.swift
@@ -7,23 +7,230 @@
 
 import UIKit
 
-class InsightsDetailViewController: UIViewController {
+import RxCocoa
+import RxSwift
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+final class InsightsDetailViewController: BaseViewController {
 
-        // Do any additional setup after loading the view.
+    private let disposeBag = DisposeBag()
+    private var viewModel: InsightsDetailViewModel
+    private var mainBlockHeight: CGFloat
+    private var mainBlockColor: UIColor
+    private var hasActionPlan: Bool
+    
+    private let customNavigationView = CommonCustomNavigationBar()
+    private let mainInsightBlock = InsightView()
+    private let memoView = InsightsDetailMemoView()
+    private lazy var actionPlanCollectionView = UICollectionView(frame: .zero, collectionViewLayout: setFlowLayout())
+    private var actionPlanButton: BottomCTAButton
+    
+    init(hasAnyActionPlan: Bool, seedId: Int) {
+        self.viewModel = InsightsDetailViewModel(hasAnyActionPlan: hasAnyActionPlan, seedId: seedId)
+        self.hasActionPlan = hasAnyActionPlan
+        
+        if hasAnyActionPlan != false {
+            mainBlockHeight = 125
+            mainBlockColor = .gray600
+            actionPlanButton = BottomCTAButton(type: .addAction)
+        } else {
+            mainBlockHeight = 108
+            mainBlockColor = .gray700
+            actionPlanButton = BottomCTAButton(type: .createAction)
+        }
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func bindViewModel() {
+        // MARK: - Bind UI With Data
+        viewModel.outputs.seedDetail
+            .bind { [weak self] data in
+                guard let self else { return }
+                let model: InsightModel = .init(name: data.caveName, insight: data.insight, date: data.lockDate, dDay: String(data.remainingDays), memo: data.memo)
+                mainInsightBlock.bindInsight(model: model)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.actionPlans
+            .bind(to: actionPlanCollectionView.rx.items(cellIdentifier: InsightsDetailActionPlanCollectionViewCell.className, cellType: InsightsDetailActionPlanCollectionViewCell.self)) { item, data, cell in
+                cell.setContent(of: data.content)
+                cell.setIsCompleted(data.isFinished)
+                cell.rxMenuButtonTapControl
+                    .bind { [weak self] _ in
+                        guard let self else { return }
+                        self.showMenuViewFromCell()
+                    }
+                    .disposed(by: cell.disposeBag)
+                cell.rxCompleteButtonTapControl
+                    .bind { [weak self] _ in
+                        guard let self else { return }
+                        self.showCompleteActionPlanView()
+                    }
+                    .disposed(by: cell.disposeBag)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.shouldHideMemoView
+            .bind { [weak self] shouldHide in
+                guard let self else { return }
+                self.memoView.isHidden = shouldHide
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.toastStatus
+            .bind { [weak self] status in
+                guard let self else { return }
+                switch status {
+                case .none:
+                    break
+                case .scrapToast(let success):
+                    self.view.showScrapToast(message: status.toastMessage, success: success)
+                case .moveSeedToast(let success):
+                    self.view.showToast(message: status.toastMessage, success: success)
+                    
+                case .editSeedToast(let success):
+                    self.view.showToast(message: status.toastMessage, success: success)
+                case .deleteSeedToast:
+                    // 이 Controller 에서 토스트가 뜨지 않음
+                    break
+                default:
+                    // 나머지는 따로 CallBack 으로 toast 띄움
+                    break
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.outputs.networkStatus
+            .bind { [weak self] status in
+                guard let self else { return }
+                switch status {
+                case .normal:
+                    break
+                case .error(let error):
+                    self.showAlert(alertText: "네트워크가 약합니다.", alertMessage: "네트워크 환경을 다시 확인해주세요.")
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        // MARK: - Button Actions - Present/Navigation
+        
+        customNavigationView.rxDoneButtonTapControl
+            .bind { [weak self] in
+                guard let self else { return }
+                self.viewModel.inputs.navigationBarMenuDidTap()
+                self.presentMenuView()
+            }
+            .disposed(by: disposeBag)
+        
+        actionPlanButton.rx.tap
+            .bind { [weak self] in
+                guard let self else { return }
+                self.showAddEtraActionPlanView()
+            }
+            .disposed(by: disposeBag)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setRegister() {
+        actionPlanCollectionView.register(InsightsDetailActionPlanCollectionViewCell.self, forCellWithReuseIdentifier: InsightsDetailActionPlanCollectionViewCell.className)
     }
-    */
+    
+    override func setStyles() {
+        view.backgroundColor = .gray700
+        
+        customNavigationView.do {
+            $0.modifyActionButtonConfigurationWithSymbol(of: "ellipsis")
+            $0.isButtonEnabled(true)
+            $0.setTitle(with: "씨앗 정보")
+            $0.setBackgroundColor(mainBlockColor)
+            $0.setButtonColorForWhen(enabled: .white000, disabled: .white000)
+        }
+        
+        memoView.do {
+            $0.isHidden = hasActionPlan
+        }
+        
+        actionPlanCollectionView.do {
+            $0.showsVerticalScrollIndicator = false
+        }
+    }
+    
+    override func setLayout() {
+        view.addSubviews(
+            customNavigationView,mainInsightBlock, memoView,
+            actionPlanButton, actionPlanCollectionView
+        )
+        
+        customNavigationView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(SizeLiterals.Screen.screenHeight * 48 / 812)
+        }
+        
+        mainInsightBlock.snp.makeConstraints {
+            $0.height.equalTo(mainBlockHeight)
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalTo(customNavigationView.snp.bottom)
+        }
+        
+        memoView.snp.makeConstraints {
+            $0.top.equalTo(mainInsightBlock.snp.bottom).offset(20)
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.height.lessThanOrEqualTo(500)
+        }
+        
+        actionPlanButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(54)
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.height.equalTo(50)
+        }
+        
+        actionPlanCollectionView.snp.makeConstraints {
+            $0.top.equalTo(mainInsightBlock.snp.bottom).offset(20)
+            $0.bottom.equalTo(actionPlanButton.snp.top).offset(-5)
+            $0.horizontalEdges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension InsightsDetailViewController {
+    
+    private func setFlowLayout() -> UICollectionViewFlowLayout {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .vertical
+        flowLayout.minimumLineSpacing = 12
+        flowLayout.itemSize = CGSize(width: view.frame.size.width - 36, height: 100)
+        return flowLayout
+    }
+    
+    private func showMenuViewFromCell() {
+        
+    }
+    
+    private func showEditActionPlanView() {
+        
+    }
+    
+    private func showCompleteActionPlanView() {
+        
+    }
+    
+    private func showDeleteActionPlanPopView() {
+        
+    }
+    
+    private func presentMenuView() {
+        
+    }
+    
+    private func showAddEtraActionPlanView() {
+        
+    }
+    
+    private func showEditActionPlanview() {
+        
+    }
 }
--- Growthook/Growthook/Presentation/InsightsDetail/Controllers/InsigthDetailActionPlanSheetViewController.swift
+++ Growthook/Growthook/Presentation/InsightsDetail/Controllers/InsigthDetailActionPlanSheetViewController.swift
@@ -7,23 +7,234 @@
 
 import UIKit
 
-class InsigthDetailActionPlanSheetViewController: UIViewController {
+import RxCocoa
+import RxSwift
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+enum ActionPlanSheetPurpose {
+    case create
+    case edit(actionPlanId: Int)
+    
+    var title: String {
+        switch self {
+        case .create:
+            return "액션 플랜 추가"
+        case .edit:
+            return "액션 플랜 수정"
+        }
+    }
+}
 
-        // Do any additional setup after loading the view.
+final class InsigthDetailActionPlanSheetViewController: BaseViewController {
+
+    private let titleLabel = UILabel()
+    private let exitButton = UIButton()
+    private let textView = TextViewBlockWithTitle(placeholder: I18N.ActionList.insightDetailPlaceholder, maxLength: 40)
+    private let doneButton = UIButton()
+    private let loadingView = FullCoverLoadingView()
+    
+    private let disposeBag = DisposeBag()
+    private var savedContent: String?
+    private let originalContent: String?
+    private let isButtonEnabled = BehaviorRelay(value: false)
+    private var purpose: ActionPlanSheetPurpose
+    private let viewModel: InsightsDetailViewModel
+    private var newContentText: String = ""
+    private var keyboardHeight: CGFloat = 0
+    
+    init(purpose: ActionPlanSheetPurpose, viewModel: InsightsDetailViewModel, originalContentText: String?) {
+        self.viewModel = viewModel
+        self.savedContent = originalContentText
+        self.originalContent = originalContentText
+        self.purpose = purpose
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let text = savedContent {
+            textView.textViewBlock.text = text
+        }
+        setDataBind()
+        setNotificationForKeyboardHeight()
     }
-    */
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        removeNotification()
+    }
+    
+    override func setStyles() {
+        view.backgroundColor = .gray600
+        
+        titleLabel.do {
+            $0.font = .fontGuide(.body1_reg)
+            $0.textColor = .white000
+            $0.text = purpose.title
+        }
+        
+        exitButton.do {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 28, weight: .bold)
+            $0.setImage(UIImage(systemName: "xmark", withConfiguration: configuration), for: .normal)
+            $0.tintColor = .white000
+        }
+        
+        doneButton.do {
+            $0.setTitle("완료", for: .normal)
+            $0.makeCornerRound(radius: 10)
+        }
+        
+        loadingView.do {
+            $0.isHidden = true
+        }
+    }
+    
+    override func setLayout() {
+        view.addSubviews(
+            titleLabel, exitButton, textView,
+            doneButton, loadingView
+        )
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(21)
+        }
+        
+        exitButton.snp.makeConstraints {
+            $0.top.trailing.equalToSuperview().inset(18)
+            $0.size.equalTo(30)
+        }
+        
+        textView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(110)
+            $0.top.equalToSuperview().inset(80)
+        }
+        
+        doneButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(20)
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.height.equalTo(50)
+        }
+        
+        loadingView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
+extension InsigthDetailActionPlanSheetViewController {
+    
+    private func setDataBind() {
+        textView.textViewBlock.rx.text
+            .bind { [weak self] text in
+                guard let self else { return }
+                guard let text else { return }
+                self.newContentText = text
+                print(self.newContentText)
+                if text == I18N.ActionList.insightDetailPlaceholder {
+                    self.isButtonEnabled.accept(false)
+                } else if text.isEmpty {
+                    self.isButtonEnabled.accept(false)
+                } else {
+                    self.isButtonEnabled.accept(true)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        textView.textViewBlock.rxEditingAction
+            .bind { [weak self] events in
+                guard let self else { return }
+                switch events {
+                case .editingDidBegin:
+                    self.moveButtonUp()
+                case .editingDidEnd:
+                    self.moveButtonDown()
+                default:
+                    break
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        isButtonEnabled
+            .bind { [weak self] enabled in
+                guard let self else { return }
+                self.doneButton.isEnabled = enabled
+                self.doneButton.backgroundColor = enabled ? .green400: .gray500
+                self.doneButton.setTitleColor(
+                    enabled ? .white000: .gray300,
+                    for: .normal
+                )
+            }
+            .disposed(by: disposeBag)
+        
+        doneButton.rx.tap
+            .bind { [weak self] in
+                guard let self else { return }
+                self.loadingView.isHidden = false
+                switch self.purpose {
+                case .create:
+                    self.viewModel.inputs.addSingleNewAction(newActionPlanText: self.newContentText) { success in
+                        if success != false {
+                            self.dismiss(animated: true)
+                        } else {
+                            self.loadingView.isHidden = true
+                        }
+                    }
+                case .edit(let actionPlanId):
+                    if newContentText == originalContent {
+                        self.dismiss(animated: true)
+                        return
+                    }
+                    
+                    self.viewModel.inputs.editActionPlan(actionPlanId: actionPlanId, editedActionPlanText: self.newContentText) { success in
+                        if success != false {
+                            self.dismiss(animated: true)
+                        } else {
+                            self.loadingView.isHidden = true
+                        }
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+extension InsigthDetailActionPlanSheetViewController {
+    
+    // 전체 560
+    private func moveButtonUp() {
+        doneButton.snp.remakeConstraints {
+            $0.bottom.equalToSuperview().inset(20+keyboardHeight)
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.height.equalTo(50)
+        }
+    }
+    
+    private func moveButtonDown() {
+        doneButton.snp.remakeConstraints {
+            $0.bottom.equalToSuperview().inset(20)
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.height.equalTo(50)
+        }
+    }
+    
+    private func setNotificationForKeyboardHeight() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    private func removeNotification() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc
+    private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let keyboardHeight = keyboardSize.height
+            self.keyboardHeight = keyboardHeight
+        }
+    }
 }
