3982aefbaf53d73e8884fc4ab900194d28733df7
PecanPie <hidra0321@naver.com>
2024-01-14 23:41:43 +0900

    [Feat] - #55 리뷰 구현

--- Growthook/Growthook/Presentation/InsightsDetail/Controllers/InsigthDetailActionPlanReviewSheetViewController.swift
+++ Growthook/Growthook/Presentation/InsightsDetail/Controllers/InsigthDetailActionPlanReviewSheetViewController.swift
@@ -7,23 +7,229 @@
 
 import UIKit
 
-class InsigthDetailActionPlanReviewSheetViewController: UIViewController {
+import RxCocoa
+import RxSwift
+
+final class InsigthDetailActionPlanReviewSheetViewController: BaseViewController {
+
+    private let titleLabel = UILabel()
+    private let exitButton = UIButton()
+    private let textView = TextViewBlockWithTitle(placeholder: I18N.ActionList.insightDetailReviewPlaceholder, maxLength: 300)
+    private let saveButton = UIButton()
+    private let skipButton = UIButton()
+    private let loadingView = FullCoverLoadingView()
+    
+    private let disposeBag = DisposeBag()
+    private let isButtonEnabled = BehaviorRelay(value: false)
+    private let viewModel: InsightsDetailViewModel
+    private var keyboardHeight: CGFloat = 0
+    private var newContentText: String = ""
+    private var actionPlanId: Int
+
+    init(viewModel: InsightsDetailViewModel, actionPlanId: Int) {
+        self.viewModel = viewModel
+        self.actionPlanId = actionPlanId
+        super.init(nibName: nil, bundle: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setDataBind()
+        setNotificationForKeyboardHeight()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.textView.becomeFirstResponder()
     }
-    */
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        removeNotification()
+    }
 
+    override func setStyles() {
+        view.backgroundColor = .gray600
+        
+        titleLabel.do {
+            $0.font = .fontGuide(.body1_reg)
+            $0.textColor = .white000
+            $0.text = "리뷰 작성"
+        }
+        
+        exitButton.do {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 28, weight: .bold)
+            $0.setImage(UIImage(systemName: "xmark", withConfiguration: configuration), for: .normal)
+            $0.tintColor = .white000
+        }
+        
+        saveButton.do {
+            $0.setTitle("저장하기", for: .normal)
+            $0.makeCornerRound(radius: 10)
+        }
+        
+        skipButton.do {
+            $0.setTitle("안 쓸래요", for: .normal)
+            $0.backgroundColor = .gray600
+        }
+        
+        loadingView.do {
+            $0.isHidden = true
+        }
+    }
+    
+    override func setLayout() {
+        view.addSubviews(
+            titleLabel, exitButton, skipButton,
+            saveButton, textView, loadingView
+        )
+        
+        titleLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().inset(21)
+        }
+        
+        exitButton.snp.makeConstraints {
+            $0.top.trailing.equalToSuperview().inset(18)
+            $0.size.equalTo(30)
+        }
+        
+        skipButton.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(30)
+            $0.width.equalTo(100)
+            $0.height.equalTo(25)
+        }
+        
+        saveButton.snp.makeConstraints {
+            $0.bottom.equalTo(skipButton.snp.top).offset(-17)
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.height.equalTo(50)
+        }
+        
+        textView.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+            $0.top.equalToSuperview().inset(80)
+            $0.bottom.equalTo(saveButton.snp.top).offset(-80)
+        }
+        
+        loadingView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension InsigthDetailActionPlanReviewSheetViewController {
+    
+    private func setDataBind() {
+        textView.textViewBlock.rx.text
+            .bind { [weak self] text in
+                guard let self else { return }
+                guard let text else { return }
+                self.newContentText = text
+                print(self.newContentText)
+                if text == I18N.ActionList.insightDetailReviewPlaceholder {
+                    self.isButtonEnabled.accept(false)
+                } else if text.isEmpty {
+                    self.isButtonEnabled.accept(false)
+                } else {
+                    self.isButtonEnabled.accept(true)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        textView.textViewBlock.rxEditingAction
+            .bind { [weak self] events in
+                guard let self else { return }
+                switch events {
+                case .editingDidBegin:
+                    self.moveButtonUp()
+    
+                case .editingDidEnd:
+                    self.moveButtonDown()
+                default:
+                    break
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        isButtonEnabled
+            .bind { [weak self] enabled in
+                guard let self else { return }
+                self.saveButton.isEnabled = enabled
+                self.saveButton.backgroundColor = enabled ? .green400: .gray500
+                self.saveButton.setTitleColor(
+                    enabled ? .white000: .gray300,
+                    for: .normal
+                )
+            }
+            .disposed(by: disposeBag)
+        
+        saveButton.rx.tap
+            .bind { [weak self] in
+                guard let self else { return }
+                self.loadingView.isHidden = false
+                self.viewModel.inputs.completeActionPlan(withReviewOf: self.newContentText, actionPlanIdOf: self.actionPlanId) { success in
+                    if success != false {
+                        self.dismiss(animated: true)
+                    } else {
+                        self.loadingView.isHidden = true
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        skipButton.rx.tap
+            .bind { [weak self] in
+                guard let self else { return }
+                self.loadingView.isHidden = false
+                self.viewModel.inputs.completeActionPlan(withReviewOf: "", actionPlanIdOf: self.actionPlanId) { success in
+                    if success != false {
+                        self.dismiss(animated: true)
+                    } else {
+                        self.loadingView.isHidden = true
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+}
+
+extension InsigthDetailActionPlanReviewSheetViewController {
+    
+    // 전체 ... 몇이지
+    private func moveButtonUp() {
+        saveButton.snp.remakeConstraints {
+            $0.bottom.equalToSuperview().inset(20+keyboardHeight)
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.height.equalTo(50)
+        }
+    }
+    
+    private func moveButtonDown() {
+        saveButton.snp.remakeConstraints {
+            $0.bottom.equalToSuperview().inset(20)
+            $0.horizontalEdges.equalToSuperview().inset(18)
+            $0.height.equalTo(50)
+        }
+    }
+    
+    private func setNotificationForKeyboardHeight() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    private func removeNotification() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+    }
+    
+    @objc 
+    private func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let keyboardHeight = keyboardSize.height
+            self.keyboardHeight = keyboardHeight
+        }
+    }
 }
