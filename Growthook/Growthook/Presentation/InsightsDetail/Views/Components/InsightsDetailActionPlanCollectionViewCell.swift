3f35b87be899be737259ac1f445045b91ff4e0c2
PecanPie <hidra0321@naver.com>
2024-01-14 23:40:58 +0900

    [Feat] - #55 Components 추가

--- Growthook/Growthook/Presentation/InsightsDetail/Views/Components/InsightsDetailActionPlanCollectionViewCell.swift
+++ Growthook/Growthook/Presentation/InsightsDetail/Views/Components/InsightsDetailActionPlanCollectionViewCell.swift
@@ -7,6 +7,107 @@
 
 import UIKit
 
-class InsightsDetailActionPlanCollectionViewCell: UICollectionViewCell {
+import RxCocoa
+import RxSwift
+
+final class InsightsDetailActionPlanCollectionViewCell: UICollectionViewCell {
     
+    private let seedImageView = UIImageView()
+    private let contentLabel = UILabel()
+    private let menuButton = UIButton()
+    private let completeButton = UIButton()
+    
+    let disposeBag = DisposeBag()
+    lazy var rxMenuButtonTapControl: ControlEvent<Void> = menuButton.rx.tap
+    lazy var rxCompleteButtonTapControl: ControlEvent<Void> = completeButton.rx.tap
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setStyles()
+        setLayout()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        contentLabel.text = nil
+        completeButton.isEnabled = false
+        completeButton.backgroundColor = .gray400
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension InsightsDetailActionPlanCollectionViewCell {
+
+    private func setStyles() {
+        backgroundColor = .gray900
+        makeCornerRound(radius: 12)
+        makeBorder(width: 1, color: .gray300)
+        
+        seedImageView.do {
+            $0.image = ImageLiterals.Home.btn_scrap_light_off
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        contentLabel.do {
+            $0.font = .fontGuide(.body3_bold)
+            $0.textColor = .white000
+            $0.numberOfLines = 2
+        }
+        
+        menuButton.do {
+            $0.setImage(UIImage(named: "EllipsisWith2Dots"), for: .normal)
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        completeButton.do {
+            $0.setTitle("완료하기", for: .normal)
+            $0.setTitleColor(.white000, for: .normal)
+            $0.setBackgroundColor(.green400, for: .normal)
+            $0.makeCornerRound(radius: 15)
+        }
+    }
+    
+    private func setLayout() {
+        addSubviews(
+            seedImageView, contentLabel, menuButton,
+            completeButton
+        )
+        
+        seedImageView.snp.makeConstraints {
+            $0.size.equalTo(48)
+            $0.top.leading.equalToSuperview()
+        }
+        
+        contentLabel.snp.makeConstraints {
+            $0.leading.equalTo(seedImageView.snp.trailing)
+            $0.top.equalToSuperview().inset(14)
+            $0.trailing.equalToSuperview().inset(85)
+        }
+        
+        menuButton.snp.makeConstraints {
+            $0.top.trailing.equalToSuperview()
+            $0.size.equalTo(48)
+        }
+        
+        completeButton.snp.makeConstraints {
+            $0.trailing.bottom.equalToSuperview().inset(12)
+            $0.height.equalTo(30)
+            $0.width.equalTo(68)
+        }
+    }
+}
+
+extension InsightsDetailActionPlanCollectionViewCell {
+    
+    func setContent(of content: String) {
+        contentLabel.text = content
+    }
+    
+    func setIsCompleted(_ completed: Bool) {
+        completeButton.isEnabled = !completed
+        completeButton.backgroundColor = completed ? .gray400: .green400
+    }
 }
--- Growthook/Growthook/Presentation/InsightsDetail/Views/InsightsDetailMemoView.swift
+++ Growthook/Growthook/Presentation/InsightsDetail/Views/InsightsDetailMemoView.swift
@@ -7,14 +7,83 @@
 
 import UIKit
 
-class InsightsDetailMemoView: UIView {
+final class InsightsDetailMemoView: BaseView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private let memoContent = UILabel()
+    private let referenceTitle = UILabel()
+    private let referenceUrlTitle = UILabel()
+    private let dividerView = UIView()
+    private let referenceStackView = UIStackView()
+
+    override func setStyles() {
+        backgroundColor = .clear
+        
+        memoContent.do {
+            $0.numberOfLines = 0
+            $0.textColor = .white000
+            $0.font = .fontGuide(.body3_reg)
+        }
+        
+        referenceStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 10
+            $0.makeCornerRound(radius: 5)
+            $0.backgroundColor = .gray500
+        }
+        
+        referenceTitle.do {
+            $0.numberOfLines = 1
+            $0.font = .fontGuide(.detail2_bold)
+            $0.textColor = .gray200
+        }
+        
+        referenceUrlTitle.do {
+            $0.numberOfLines = 1
+            $0.font = .fontGuide(.detail2_reg)
+            $0.textColor = .gray200
+        }
+        
+        dividerView.do {
+            $0.backgroundColor = .gray200
+        }
     }
-    */
+ 
+    override func setLayout() {
+        self.addSubviews(memoContent, referenceStackView)
+        referenceStackView.addArrangedSubviews(
+            referenceTitle, dividerView, referenceUrlTitle
+        )
+        
+        memoContent.snp.makeConstraints {
+            $0.top.horizontalEdges.equalToSuperview()
+        }
+        
+        referenceStackView.snp.makeConstraints {
+            $0.top.equalTo(memoContent.snp.bottom).offset(16)
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(26)
+            $0.width.lessThanOrEqualTo(frame.size.width-50)
+            $0.bottom.equalToSuperview()
+        }
+    
+        dividerView.snp.makeConstraints {
+            $0.width.equalTo(1)
+            $0.height.equalTo(14)
+        }
+    }
+}
 
+extension InsightsDetailMemoView {
+    
+    func setMemoContent(with content: String) {
+        memoContent.text = content
+    }
+    
+    func setReferenceContent(reference: String, url: String?) {
+        referenceTitle.text = reference
+        referenceUrlTitle.text = url
+        if url == nil {
+            dividerView.removeFromSuperview()
+        }
+    }
 }
