7e1a338242e891bc381b231c48233026206b8bce
PecanPie <hidra0321@naver.com>
2024-01-25 15:58:49 +0900

    [Feat] - #74 Token 만료로 인한 재시도 Operator 를 추가했습니다.

--- Growthook/Growthook/Global/Extension/Observable+.swift
+++ Growthook/Growthook/Global/Extension/Observable+.swift
@@ -28,7 +28,6 @@ extension Observable where Element == Response {
                     observer.onError(ServiceError.invalidResponse(responseCode: element.statusCode, message: element.description))
                 case 401:
                     observer.onError(ServiceError.tokenExpired)
-                    TokenManager.shared.refreshNewToken()
                 case 403...499:
                     // TODO: 여기서 지금 에러 메세지가 400 과 401 을 제외한 에러에 대한 정리본을 받지 못해 이렇게 퉁쳐놉니다.
                     observer.onError(ServiceError.invalidResponse(responseCode: element.statusCode, message: element.description))
@@ -55,6 +54,26 @@ extension Observable where Element == Response {
                     observer.onError(error)
                 }
                 return Disposables.create()
+            }
+        }
+    }
+
+}
+
+extension ObservableType where Element == Response {
+    func retryOnTokenExpired(maxAttempts: Int = 2) -> Observable<Response> {
+        return self.retry { errors in
+            errors.enumerated().flatMap { (attempt, error) -> Observable<Void> in
+                print("🔥")
+                if let serviceError = error as? ServiceError, serviceError == .tokenExpired {
+                    if attempt < maxAttempts - 1 {
+                        // 토큰 갱신 로직을 실행하고, 성공하거나 실패해도 Void를 방출
+                        return TokenManager.shared.refreshNewToken()
+                            .catchAndReturn(()) // 토큰 갱신 실패 시 오류 대신 Void 방출
+                    }
+                }
+                // 최대 재시도 횟수를 초과하거나 다른 오류의 경우 오류를 방출
+                return Observable.error(error)
             }
         }
     }
