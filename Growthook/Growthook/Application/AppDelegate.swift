6a3faf21d9509c94c1bc1178d50c9dd801521d34
kwonjeong <kwonjeong9470@gmail.com>
2024-01-26 15:20:18 +0900

    [Feat] #95 - 세로모드 고정

--- Growthook/Growthook/Application/AppDelegate.swift
+++ Growthook/Growthook/Application/AppDelegate.swift
@@ -46,5 +46,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.deviceHeight = height
         self.deviceWidth = width
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        // 세로방향 고정
+        return UIInterfaceOrientationMask.portrait
+    }
 }
 
