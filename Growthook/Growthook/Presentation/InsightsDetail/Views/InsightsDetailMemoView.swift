5997acd5bbb97d20eb5969ddee8484d8de21a9c6
frohsch <kminjoo1213@gmail.com>
2024-03-11 10:30:36 +0900

    [Feat] #129 - Add url verify method

--- Growthook/Growthook/Presentation/InsightsDetail/Views/InsightsDetailMemoView.swift
+++ Growthook/Growthook/Presentation/InsightsDetail/Views/InsightsDetailMemoView.swift
@@ -106,4 +106,13 @@ extension InsightsDetailMemoView {
             backgroundBoxView.removeFromSuperview()
         }
     }
+    
+    func verifyUrl (urlString: String?) -> Bool {
+        if let urlString = urlString {
+            if let url = NSURL(string: urlString) {
+                return UIApplication.shared.canOpenURL(url as URL)
+            }
+        }
+        return false
+    }
 }
